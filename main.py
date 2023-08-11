86e2c517367b5484c0a3cfa781470feebfbbae47
Ethan Silverstein <ethanlsilverstein@gmail.com>
2023-08-11 16:13:08 -0400

    Fixed old code

--- main.py
+++ main.py
@@ -9,30 +9,6 @@ from langchain.vectorstores import Chroma
 
 openai_api_key = os.environ['OPENAI_KEY']
 
-class StreamHandler(BaseCallbackHandler):
-    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
-        self.container = container
-        self.text = initial_text
-
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.markdown(self.text)
-
-
-class PrintRetrievalHandler(BaseCallbackHandler):
-    def __init__(self, container):
-        self.container = container.expander("Context Retrieval")
-
-    def on_retriever_start(self, query: str, **kwargs):
-        self.container.write(f"**Question:** {query}")
-
-    def on_retriever_end(self, documents, **kwargs):
-        # self.container.write(documents)
-        for idx, doc in enumerate(documents):
-            source = os.path.basename(doc.metadata["source"])
-            self.container.write(f"**Document {idx} from {source}**")
-            self.container.markdown(doc.page_content)
-
 books = {
     'Alice In Wonderland': ['embeddingsbooks/Alice_In_Wonderland.txt', ['Alice', 'The White Rabbit', 'The Queen of Hearts', 'The King of Hearts', 'The Mad Hatter']],
     'A Doll\'s House': ['embeddingsbooks/Doll_House.txt', ['Nora Helmer', 'Torvald Helmer', 'Dr. Rank', 'Mrs. Linde', 'Krogstad']],
